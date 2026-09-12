1096dbd234e49f7da065fa23ee174e7449a589e5
𝑬𝑵𝑮. 𝑬𝑳𝒀𝑨𝑺 𝑨𝑳 𝑺𝑯𝑨𝑬𝑩𝑰 <em775377979@gmail.com>
2026-09-12 21:22:51 +0300

    feat(student): load authenticated student shell styles

--- apps/student-web/src/main.tsx
+++ apps/student-web/src/main.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./styles.css";
 import "./stage14.css";
 import "./foundation.css";
+import "./student-shell.css";
 import { registerStudentServiceWorker } from "./pwa";
 import { StudentRouter } from "./router";
 
