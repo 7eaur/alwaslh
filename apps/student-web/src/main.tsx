32345c743f85ea38c9f80242313040a0f93cde87
𝑬𝑵𝑮. 𝑬𝑳𝒀𝑨𝑺 𝑨𝑳 𝑺𝑯𝑨𝑬𝑩𝑰 <em775377979@gmail.com>
2026-09-13 03:49:11 +0300

    feat(student): load interaction affordance layer

--- apps/student-web/src/main.tsx
+++ apps/student-web/src/main.tsx
@@ -12,6 +12,7 @@ import "./student-entry.css";
 import "./student-session.css";
 import "./student-library-account.css";
 import "./student-future.css";
+import "./student-affordance.css";
 import "./student-motion.css";
 import { registerStudentServiceWorker } from "./pwa";
 import { StudentRouter } from "./router";
