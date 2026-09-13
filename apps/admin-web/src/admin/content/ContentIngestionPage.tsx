d184ec9efaa17a89710cd9e9798c8972cd1236fb
𝑬𝑵𝑮. 𝑬𝑳𝒀𝑨𝑺 𝑨𝑳 𝑺𝑯𝑨𝑬𝑩𝑰 <em775377979@gmail.com>
2026-09-13 21:19:14 +0300

    refactor(admin): add content ingestion feature seam

---
+++ apps/admin-web/src/admin/content/ContentIngestionPage.tsx
@@ -0,0 +1,5 @@
+import { ContentIngestionWorkspace } from "../../ContentIngestionWorkspace";
+
+export function ContentIngestionPage({ onSessionExpired }: { onSessionExpired: () => void }) {
+  return <ContentIngestionWorkspace onSessionExpired={onSessionExpired} />;
+}
