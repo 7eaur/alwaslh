e7a30a3b9c304c8f7db5061553ce1c73d664c315
𝑬𝑵𝑮. 𝑬𝑳𝒀𝑨𝑺 𝑨𝑳 𝑺𝑯𝑨𝑬𝑩𝑰 <em775377979@gmail.com>
2026-09-14 05:04:09 +0300

    fix(student): avoid duplicate assessment stylesheet import

--- apps/student-web/src/main.tsx
+++ apps/student-web/src/main.tsx
@@ -11,7 +11,6 @@ import "./student-learning-v2.css";
 import "./student-reader-v2.css";
 import "./foundation.css";
 import "./assessment-integration.css";
-import "./assessment.css";
 import "./student-practice-v2.css";
 import "./student-entry.css";
 import "./student-entry-v2.css";
