2dd1ca2a94f03b8299bc2f8759f634c56fc10f78
𝑬𝑵𝑮. 𝑬𝑳𝒀𝑨𝑺 𝑨𝑳 𝑺𝑯𝑨𝑬𝑩𝑰 <em775377979@gmail.com>
2026-09-15 14:33:13 +0300

    refactor(admin): use shared client in lesson publication

--- apps/admin-web/src/admin/content/LessonPublicationPanel.tsx
+++ apps/admin-web/src/admin/content/LessonPublicationPanel.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useState } from "react";
-import { ApiRequestError, isMissingSessionError } from "../../admin-api";
+import { ApiRequestError, isMissingSessionError } from "../../shared/api/client";
 import {
   type AdminLessonContentState,
   type LessonContentPublicationAction,
