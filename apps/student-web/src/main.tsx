fb23778d5ed1841e8b80b313b29e7f7d84383e78
𝑬𝑵𝑮. 𝑬𝑳𝒀𝑨𝑺 𝑨𝑳 𝑺𝑯𝑨𝑬𝑩𝑰 <em775377979@gmail.com>
2026-09-15 04:50:10 +0300

    style(student): load approved Home reference layer

--- apps/student-web/src/main.tsx
+++ apps/student-web/src/main.tsx
@@ -23,6 +23,7 @@ import "./student-affordance.css";
 import "./student-motion.css";
 import "./student-visual-parity.css";
 import "./student-visual-parity-qa.css";
+import "./student-home-reference.css";
 import { registerStudentServiceWorker } from "./pwa";
 import { StudentRouter } from "./router";
 
